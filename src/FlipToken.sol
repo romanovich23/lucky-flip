e7d11a1e92554f8ffe7e7cece9731b9031d45182
Javier Román <javier.rmgz@gmail.com>
2024-07-12 21:11:46 +0200

    Refactor OZ library path

--- src/FlipToken.sol
+++ src/FlipToken.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.20;
 
-import {ERC20} from "@openzeppelin-contracts/token/ERC20/ERC20.sol";
+import {ERC20} from "@openzeppelin-contracts-5.0.2/token/ERC20/ERC20.sol";
 
 contract FlipToken is ERC20 {
     constructor() ERC20("FlipToken", "FLP") {}
